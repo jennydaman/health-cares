a4fc93a0a07eac2f9c2c6e506f4de6224f33dc26
Jennings Zhang <jennydaman@users.noreply.github.com>
2017-09-10 10:02:51 -0400

    Restore bubblechart & add style

--- hospitaldb/server.js
+++ hospitaldb/server.js
@@ -26,6 +26,7 @@ server = http.createServer(function (request, response) {
         '.html': 'text/html',
         '.js': 'text/javascript',
         '.json': 'application/json',
+        '.css': 'text/css',        
     };
 
     contentType = mimeTypes[extname] || 'application/octet-stream';
